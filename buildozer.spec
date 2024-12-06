1890e9c8d6bf3bf686bcc49e24a9f3c537bd7bae
testcolab-web <jnv.11thscience@gmail.com>
2024-12-07 00:08:28 +0530

    Add files via upload

---
+++ buildozer.spec
@@ -0,0 +1,15 @@
+
+[app]
+title = TestGPT
+package.name = testgpt
+package.domain = com.mdselim
+source.dir = .
+source.include_exts = py,png,jpg,kv,atlas
+version = 0.1
+requirements = kivy
+orientation = portrait
+fullscreen = 1
+
+[buildozer]
+log_level = 2
+warn_on_root = 1
---
+++ main.py
@@ -0,0 +1,16 @@
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.webview import WebView
+
+class WebApp(App):
+    def build(self):
+        # Create a full-screen WebView
+        layout = BoxLayout(orientation='vertical')
+        webview = WebView()
+        webview.url = "https://testgpt-6u1q.onrender.com/"  # Replace with your desired URL
+        layout.add_widget(webview)
+        return layout
+
+if __name__ == '__main__':
+    WebApp().run()
